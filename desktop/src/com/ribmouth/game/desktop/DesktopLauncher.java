2059c8db3c15034c43acf8f2b9da128a973da6f3
Jarno Michiels <wefknrise@gmail.com>
2017-10-31 12:18:20 +0100

    Initial commit

---
+++ desktop/src/com/ribmouth/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ribmouth.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ribmouth.game.Omo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Omo(), config);
+	}
+}
