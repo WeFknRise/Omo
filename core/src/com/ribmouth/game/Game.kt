f383bd7d65f2f2dfbc345158c038c02e45b60e0d
Jarno Michiels <wefknrise@gmail.com>
2017-10-31 12:58:21 +0100

    refactored java classes to kotlin classes

---
+++ core/src/com/ribmouth/game/Game.kt
@@ -0,0 +1,18 @@
+package com.ribmouth.game
+
+import com.badlogic.gdx.ApplicationAdapter
+
+class Game : ApplicationAdapter() {
+
+    override fun create() {
+
+    }
+
+    override fun render() {
+
+    }
+
+    override fun dispose() {
+
+    }
+}
