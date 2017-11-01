1abe948a7416f64f1197709c4ec73001960ea13d
Jarno Michiels <wefknrise@gmail.com>
2017-11-01 19:48:46 +0100

    added glowing

--- core/src/com/ribmouth/game/states/PlayState.kt
+++ core/src/com/ribmouth/game/states/PlayState.kt
@@ -2,19 +2,34 @@ package com.ribmouth.game.states
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils.random
+import com.badlogic.gdx.utils.Array
 import com.ribmouth.game.Game
 import com.ribmouth.game.handlers.GameStateManager
 import com.ribmouth.game.ui.SizingTile
+import com.ribmouth.game.ui.Tile
 
 /**
  * Created by RibMouth on 31/10/2017.
  */
 class PlayState(gsm: GameStateManager) : GameState(gsm) {
+    companion object {
+        const val SHOW_TIMER: Float = 4.0f
+        const val GLOW_ON_SECOND: Float = 3.0f
+    }
+
     object MultiTouch {
         const val MAX_FINGERS: Int = 2
     }
 
     private var tiles: ArrayList<ArrayList<SizingTile>> = arrayListOf()
+    //user selected
+    private var selected: Array<Tile> = Array()
+    //solution
+    private var finished: Array<Tile> = Array()
+
+    private var showing: Boolean = true
+    private var showTimer: Float = 0.0f
 
     private var tileSize: Float = -1f
     private var boardOffset: Float = -1f
@@ -22,11 +37,12 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
 
     init {
         createBoard(4, 4)
+        createFinished(3)
     }
 
     override fun handleInput() {
         for (i in 0 until MultiTouch.MAX_FINGERS) {
-            if (Gdx.input.isTouched(i)) {
+            if (!showing && Gdx.input.isTouched(i)) {
                 mouse.x = Gdx.input.getX(i).toFloat()
                 mouse.y = Gdx.input.getY(i).toFloat()
                 cam.unproject(mouse)
@@ -34,11 +50,33 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
                 if (mouse.y >= boardOffset && mouse.y <= boardOffset + boardHeight) {
                     val row: Int = ((mouse.y - boardOffset) / tileSize).toInt()
                     val col: Int = (mouse.x / tileSize).toInt()
-
-                    if (row >= 0 && row < tiles.count() && col >= 0 && col < tiles[row].count()) {
-                        val tile = tiles[row][col]
+                    val tile = tiles[row][col]
+                    if(!tile.selected) {
                         tile.selected = true
+                        selected.add(tile)
+
+                        if(isFinished()) {
+                            createBoard(4, 4)
+                            createFinished(3)
+                        }
                     }
+
+
+                    /*if (row >= 0 && row < tiles.count() && col >= 0 && col < tiles[row].count()) {
+                        val tile = tiles[row][col]
+                        tile.selected = !tile.selected
+
+                        if(tile.selected) {
+                            selected.add(tile)
+                        } else {
+                            selected.removeValue(tile, true)
+                        }
+                    }
+
+                    if(isFinished()) {
+                        createBoard(5, 5)
+                        createFinished(3)
+                    }*/
                 }
             }
         }
@@ -46,6 +84,7 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
 
     override fun update(dt: Float) {
         handleInput()
+        showObjective(dt)
 
         // tiles
         for (row in 0 until tiles.count()) {
@@ -69,6 +108,38 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
         sb.end()
     }
 
+    private fun showObjective(dt: Float) {
+        if(showing) {
+            showTimer += dt
+
+            if(showTimer > GLOW_ON_SECOND) {
+                if(showTimer % 0.15f < 0.07f) {
+                    for (f in finished) {
+                        f.selected = true
+                    }
+                } else {
+                    for (f in finished) {
+                        f.selected = false
+                    }
+                }
+            }
+
+            if(showTimer > SHOW_TIMER) {
+                showing = false
+
+                for (tile in finished) {
+                    tile.selected = false
+                }
+            }
+        }
+    }
+
+    private fun isFinished(): Boolean {
+        if(finished.count() != selected.count()) return false
+
+        return finished.all { selected.contains(it, true) }
+    }
+
     private fun createBoard(numRow: Int, numCol: Int) {
         // create tiles based on fixed setting of GRID
         tileSize = Game.WIDTH / (if (numCol >= numRow) numCol else numRow)
@@ -83,6 +154,27 @@ class PlayState(gsm: GameStateManager) : GameState(gsm) {
                 tiles[row].add(col, SizingTile(col * tileSize + tileSize / 2, row * tileSize + boardOffset + tileSize / 2, tileSize, tileSize))
                 tiles[row][col].timer = -((numRow - row) * 0.02f) - col * 0.05f
             }
+        }
+    }
+
+    private fun createFinished(numTilesToLight: Int) {
+        selected.clear()
+        finished.clear()
+
+        showing = true
+        showTimer = 0f
+
+        for (i in 0 until numTilesToLight) {
+            var row: Int
+            var col: Int
+
+            do {
+                row = random(tiles.count() - 1)
+                col = random(tiles[row].count() - 1)
+            } while(finished.contains(tiles[row][col], true))
+
+            finished.add(tiles[row][col])
+            tiles[row][col].selected = true
         }
     }
 }
